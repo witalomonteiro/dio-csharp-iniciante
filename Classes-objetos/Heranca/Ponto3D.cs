0877cc7fb4b850e207f701b9c3287f902440cfa5
Witalo Monteiro <witalo5094@gmail.com>
2020-08-29 08:38:22 -0300

    concluido 5 aula

---
+++ Classes-objetos/Heranca/Ponto3D.cs
@@ -0,0 +1,17 @@
+namespace Classes_objetos.Heranca {
+    public class Ponto3D : Ponto {
+        public int z;
+        public Ponto3D(int x, int y, int z) : base(x, y) {
+            this.z = z;
+            CalcularDistancia();
+        }
+
+        public static void Calcular() {
+            //Faz alguma coisa...
+        }
+        public override void CalcularDistancia3() {
+            //Faz outra coisa ...
+            base.CalcularDistancia3();
+        }
+    }
+}
---
+++ Classes-objetos/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Classes_objetos {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
